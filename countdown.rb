b344b94c5166a26c6e3d9262f373486c7451d882
Viviana Lugo <vivi28lu@gmail.com>
2020-09-15 03:47:01 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,3 @@
-def countdown_with_sleep (seconds)
-  while seconds > 0 
-  puts "#{seconds} SECOND(S)!"
-  seconds -= 1 
-  sleep (1)
-    end
-  return "HAPPY NEW YEAR!"
-end
-
 def countdown(number)
   while number > 0
     puts "#{number} SECOND(S)!"
@@ -15,3 +6,11 @@ def countdown(number)
  return "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleep(number)
+    while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep(1.second)
+  end
+ puts "HAPPY NEW YEAR!"
+end
