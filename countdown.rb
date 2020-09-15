9bf1e78e435e6fa515cd3a3566dd17b4def04a03
Viviana Lugo <vivi28lu@gmail.com>
2020-09-15 03:47:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,7 +10,7 @@ def countdown_with_sleep(number)
     while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
-    sleep(1.second)
+    sleep(1)
   end
- puts "HAPPY NEW YEAR!"
+ return "HAPPY NEW YEAR!"
 end
